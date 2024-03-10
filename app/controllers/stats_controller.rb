cee087a23e0d88f7d6e32e2ced3738cd57d8295c
HAKUHO <hakuho01@gmail.com>
2024-03-11 03:13:37 +0900

    SQLの大文字・小文字変更

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,18 +14,18 @@ class StatsController < ApplicationController
     sql = 'SELECT player_name, COUNT(player_name) FROM winners WHERE tournament_type = 3 GROUP BY player_name ORDER BY COUNT(player_name) DESC'
     @cheap_top_winners = Winner.find_by_sql(sql)
 
-    @count_card_name = "COUNT(cardname)"
+    @count_card_name = "count(cardname)"
 
     # ヴィンテージキューブ頻出カード
-    sql = 'SELECT cardname, COUNT(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 1 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
+    sql = 'SELECT cardname, count(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 1 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
     @vintage_frequent_cards = Card.find_by_sql(sql)
 
     # パウパーキューブ頻出カード
-    sql = 'SELECT cardname, COUNT(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 2 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
+    sql = 'SELECT cardname, count(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 2 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
     @pauper_frequent_cards = Card.find_by_sql(sql)
 
     # カスレアキューブ頻出カード
-    sql = 'SELECT cardname, COUNT(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 3 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
+    sql = 'SELECT cardname, count(cardname) FROM cards INNER JOIN winners ON cards.winner_id = winners.id WHERE tournament_type = 3 GROUP BY cardname ORDER BY COUNT(cardname) DESC LIMIT 5'
     @cheap_frequent_cards = Card.find_by_sql(sql)
   end
 end
