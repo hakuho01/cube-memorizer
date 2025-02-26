a21b6d4debe6dcc8f8507c6457dc6af2d90f528c
HAKUHO <hakuho01@gmail.com>
2025-02-26 21:22:27 +0900

    ソート順修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,8 @@
+# class RecordsController
 class RecordsController < ApplicationController
   def index
     @tournaments = Tournament.all
-    @winners = Winner.all.order(:id)
+    @winners = Winner.all.order(id: :desc)
     @tournament_type = TournamentType.all
   end
 end
